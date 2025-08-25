40e99b294a6066365b96b54471b3ac4013bd2c6e
jainbhavya2005 <jainbhavya0015@gmail.com>
2025-08-25 10:24:51 +0530

    Update q5a.cpp

--- q5a.cpp
+++ q5a.cpp
@@ -15,4 +15,21 @@ public:
     }
     int get(int i,int j) {
         if(i==j) return A[i-1];
-        else return
+        else return 0;
+    }
+    void display() {
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(i==j) cout<<A[i]<<" ";
+                else cout<<"0 ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main() {
+    Diagonal d(4);
+    d.set(1,1,5); d.set(2,2,8); d.set(3,3,9); d.set(4,4,12);
+    d.display();
+}
